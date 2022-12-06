b0be439b45773587ed89dca48c84d6606f8445bd
50^2 <alexeusgr@gmail.com>
2022-12-06 09:27:47 +0200

    replace with home.nix

--- home/configurations/alex@m3800.nix
+++ home/configurations/alex@m3800.nix
@@ -1,101 +1,13 @@
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
 {
-  unstable,
-  self,
-  ...
-}: {pkgs, ...}: {
-  config = {
-    nixpkgs.allowedUnfree = ["google-chrome" "vscode" "discord"];
-    nixpkgs.config.allowBroken = true;
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
 
-    activeProfiles = ["browsing" "development"];
-
-    dconf.enable = true;
-
-    enabledLanguages = [
-      # "agda"  # Seems as if AGDA2-mode isn't on melpa anymore
-      "clojure"
-      "cpp"
-      "elixir"
-      "erlang"
-      "go"
-      "nim"
-      "nix"
-      # "ocaml"
-      "python"
-      "rust"
-      "tex"
-    ];
-
-    programs.emacs.splashScreen = false;
-
-    home.packages = let
-      p = pkgs;
-      s = self.packages.x86_64-linux;
-    in [
-      p.handbrake
-      p.keybase-gui
-      p.minikube
-      p.gnome.gnome-tweaks
-      p.freerdp
-      p.vscode
-      p.keepassxc
-      p.ipfs
-      p.nix-output-monitor
-      p.discord
-
-      s.gnucash-de
-    ];
-
-    programs.obs-studio.enable = true;
-    programs.htop = {
-      settings = {
-        detailed_cpu_time = true;
-      };
-      # meters.right = [
-      #   { kind = "Battery"; mode = 1; }
-      #   "Tasks"
-      #   "LoadAverage"
-      #   "Uptime"
-      # ];
-    };
-
-    xsession.windowManager.awesome.autostart = [
-      "${pkgs.blueman}/bin/blueman-applet"
-      "${pkgs.networkmanagerapplet}/bin/nm-applet"
-    ];
-
-    services = {
-      keyleds.enable = true;
-      keybase.enable = true;
-      kbfs.enable = true;
-      insync.enable = true;
-      playerctld.enable = true;
-
-      restic = {
-        enable = true;
-        exclude = (map (e: "%h/${e}") [".cache" ".cabal" ".cargo" ".emacs.d/eln-cache" ".emacs.d/.cache" ".gem" ".gradle" ".hex" ".kube" ".local" ".m2" ".minikube" ".minishift" ".mix" ".mozilla" "npm" ".opam" ".rancher" ".vscode-oss" "go/pkg"]) ++ ["_build" "deps" "result" "target" ".elixir_ls" "ccls-cache" ".direnv"];
-        oneFileSystem = true;
-        compression = "max";
-        repo = "rest:https://restic.mimas.internal.nobbz.dev/nobbz";
-      };
-    };
-
-    systemd.user.services = {
-      keybase-gui = {
-        Unit = {
-          Description = "Keybase GUI";
-          Requires = ["keybase.service" "kbfs.service"];
-          After = ["keybase.service" "kbfs.service"];
-        };
-        Service = {
-          ExecStart = "${pkgs.keybase-gui}/share/keybase/Keybase";
-          PrivateTmp = true;
-          # Slice      = "keybase.slice";
-        };
-      };
-    };
+  home-manager.users.alex = {
+    /* Here goes your home-manager config, eg home.packages = [ pkgs.foo ]; */
   };
-  # environment.pathsToLink = [ "/share/zsh" ];
 }
-# /nix/store/7skqa8vxfydq7w3cix55ffvkmjb3b5da-python-2.7.18
-
